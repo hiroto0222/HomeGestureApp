e7e656be3d5c456ef1eeee30bb2454e7b934c68a
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-10-03 21:29:52 +0900

    feat: add feature extractors for hand shape

---
+++ cnn/main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 00:44:25 2021
+
+@author: chakati
+"""
+import cv2
+import numpy as np
+import os
+import tensorflow as tf
+
+## import the handfeature extractor class
+
+# =============================================================================
+# Get the penultimate layer for trainig data
+# =============================================================================
+# your code goes here
+# Extract the middle frame of each gesture video
+
+
+
+
+# =============================================================================
+# Get the penultimate layer for test data
+# =============================================================================
+# your code goes here 
+# Extract the middle frame of each gesture video
+
+
+
+
+# =============================================================================
+# Recognize the gesture (use cosine similarity for comparing the vectors)
+# =============================================================================
